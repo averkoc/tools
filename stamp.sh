7ac78eeceab844e63aaab44b39ec26d2a47982f1
averkoc <sakari.mannisto@centria.fi>
2024-09-30 20:14:32 +0300

    Update stamp.sh

--- stamp.sh
+++ stamp.sh
@@ -4,7 +4,7 @@
 # Intentionally a little bit obscure :)
 # 
 a=$(date)
-b=$(echo $a |tr " " "_")
+b=${a// /_}
 c=$(./verify $b)
 d=$(./verify $USER)
 echo "$USER completed the step at:$a check:${c: -4} ${d: -4}"
