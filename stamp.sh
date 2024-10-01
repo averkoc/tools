299570336fd749cbf24ce7a8280bd1cc619e92ac
averkoc <sakari.mannisto@centria.fi>
2024-10-01 09:58:29 +0300

    Update stamp.sh

--- stamp.sh
+++ stamp.sh
@@ -9,11 +9,9 @@ b=${a// /_}
 USER=$(whoami)
 
 if command -v md5 > /dev/null; then
-    # macOS
     HASH1=$(echo -n "$USER" | md5)
     HASH2=$(echo -n "$b" | md5)
 else
-    # Linux
     HASH1=$(echo -n "$USER" | md5sum | awk '{print $1}')
     HASH2=$(echo -n "$b" | md5sum | awk '{print $1}')
 fi
