db8531d6fc6576959fb7bfac57f84d9c7c3c650e
averkoc <sakari.mannisto@centria.fi>
2024-10-01 10:21:59 +0300

    Update stamp.sh

--- stamp.sh
+++ stamp.sh
@@ -9,11 +9,14 @@ b=${a// /_}
 USER=$(whoami)
 
 if command -v md5 > /dev/null; then
-    HASH1=$(echo -n "$USER" | md5)
-    HASH2=$(echo -n "$b" | md5)
+    # macOS
+    HASH1=$(echo -n "$b" | md5)
+    HASH2=$(echo -n "$USER{HASH1: -2}" | md5)
 else
-    HASH1=$(echo -n "$USER" | md5sum | awk '{print $1}')
-    HASH2=$(echo -n "$b" | md5sum | awk '{print $1}')
+    # Linux
+    HASH1=$(echo -n "$b" | md5sum | awk '{print $1}')
+    HASH2=$(echo -n "$USER${HASH1: -2}" | md5sum | awk '{print $1}')
+
 fi
 
-echo "$USER completed the step at:$a check:${HASH2: -4} ${HASH1: -4}"
+echo "$USER completed the step at:$a check:${HASH1: -4} ${HASH2: -4}"
